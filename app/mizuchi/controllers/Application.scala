455cc1b2e326e4ed997e654ab7765479a8f73ddb
Taylor Lodge <ubermouse894@gmail.com>
2014-10-08 15:49:00 +1300

    rename Action.action_name => name; Serve frontend

--- app/mizuchi/controllers/Application.scala
+++ app/mizuchi/controllers/Application.scala
@@ -5,13 +5,10 @@ import play.api.mvc._
 import scaldi.{ Injectable, Injector }
 import mizuchi.services.ShowService
 
-class Application(implicit inj: Injector) extends Controller with Injectable {
-  val service = inject[ShowService]
+class Application extends Controller {
 
   def index = Action {
-    play.api.db.slick.DB.withSession { implicit s =>
-      Ok(mizuchi.views.html.main("Index", service.list))
-    }
+    Ok(mizuchi.views.html.main())
   }
 
 }
