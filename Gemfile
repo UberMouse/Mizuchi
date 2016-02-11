ba36f7cdce560482144f8f11c3255e7a8e59e54e
Taylor Lodge <ubermouse894@gmail.com>
2016-02-11 17:57:36 +1300

    Setup environment and Docker

--- Gemfile
+++ Gemfile
@@ -1,28 +1,29 @@
 source 'https://rubygems.org'
 
 
-gem 'rails', '4.2.0'
-
+gem 'rails', '~> 4.2'
 gem 'rails-api'
 
-gem 'spring', :group => :development
+gem 'pg' 
 
+group :development, :test do
+  gem 'better_errors'
+  gem 'pry-rails'
+  gem 'pry-byebug'
+  gem 'pry-rescue'
+  gem 'pry-stack_explorer'
+  gem 'guard-rspec', require: false
+end
 
-gem 'sqlite3'
+group :development do
+  gem 'spring'
+  gem 'web-console', '~> 2.0'
+end
 
+group :test do
+  gem 'shoulda-matchers', '~> 3.1'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+end
 
-
-# To use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano', :group => :development
-
-# To use debugger
-# gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ spec/rails_helper.rb
@@ -0,0 +1,41 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+# Add additional requires below this line. Rails is not loaded until this point!
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  config.include(FactoryGirl::Syntax::Methods)
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    with.library :rails
+  end
+end
